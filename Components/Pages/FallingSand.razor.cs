d29608f061fa5983230d697d77ab7009b7dbc555
Owain15 <153022268+Owain15@users.noreply.github.com>
2024-07-19 02:55:18 +0100

    find a way to link timer event to rendering a componeant

--- Components/Pages/FallingSand.razor.cs
+++ Components/Pages/FallingSand.razor.cs
@@ -1,60 +1,180 @@
+using FallingSand.Components.Layout;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
 namespace FallingSand.Components.Pages
 {
 
     public partial class FallingSand
     {
-        DisplayClass Display;
+
+        int GridSizeX = 60;
+        int GridSizeY = 60;
+
+        System.Timers.Timer timer;
+        int RefreshRate = 1000;
+
+        List<(int,int)> SandList = new List<(int,int)>();
+		bool[,] gameData;
+        Pixel[,] displayData;
+
+        public int testTimerReadout = 50;
+        
+
         protected override void OnInitialized()
         {
-            Display = new DisplayClass();
+            
+            timer = new System.Timers.Timer(1);
+
+            gameData = new bool[GridSizeX,GridSizeY];
+          
+            displayData = new Pixel[GridSizeX,GridSizeY];
+
+            StartTimmer();
+
+		    InitialiseDisplay();
+           
+            //test sand. Remove.
+            gameData[10, 10] = true;
+			gameData[10, 11] = true;
+			gameData[11, 10] = true;
+			gameData[11, 11] = true;
+
+		
+			displayData = Display.GetPixelDataFromGameState(gameData);
+		}
+
+
+		void StartTimmer()
+        {
+
+            timer = new System.Timers.Timer();
+            timer.Interval = RefreshRate;
+			//timer.Elapsed += new ElapsedEventHandler(RenderTimerEvent);
+			//timer.Elapsed += async (object? sender, ElapsedEventArgs e) =>
+			//{
+			//	//Console.WriteLine("timer.Intervel Hit");
+			//	//Console.WriteLine($"timerReadOut = {testTimerReadout}");
+			//	testTimerReadout++;
+			//	await InvokeAsync(StateHasChanged);
+			//};
+			timer.AutoReset = true;
+            timer.Enabled = true;
+            
         }
 
-    }
-
-    public class DisplayClass
-    {
-        private int displyX;
-        private int displayY;  
-        public Pixel[,] pixelData;
-
-        public DisplayClass()
+      
+        public void RenderTimerEvent(Object source, System.Timers.ElapsedEventArgs e)
         {
-            displyX = 60;
-            displayY = 60;
-
-            pixelData = new Pixel[displyX,displayY];
-            InitialiseDisplay();
+			Console.WriteLine("timer.Intervel Hit");
+			Console.WriteLine($"timerReadOut = {testTimerReadout}");
+			testTimerReadout++;
+            base.ShouldRender();
+            //StateHasChanged();
         }
 
-        void InitialiseDisplay()
-        {
+		void InitialiseDisplay()
+		{
 
-            for ( int y = 0; y < displayY; y++ )
-            { 
-              for( int x = 0; x < displyX; x++ )
-                {
-                    pixelData[x, y] = new Pixel(x,y);
+			for (int y = 0; y < GridSizeY; y++)
+			{
+				for (int x = 0; x < GridSizeX; x++)
+				{
+					displayData[x, y] = new Pixel(x, y);
 
-                    //Randomize Color 
-                    Random ran = new Random();
-                    pixelData[x, y].Color.Red = ran.Next(0, 250);
-					pixelData[x, y].Color.Blue = ran.Next(0, 250);
-					pixelData[x, y].Color.Green = ran.Next(0, 250);
+					//Randomize Color 
+					Random ran = new Random();
+					displayData[x, y].Color.Red = ran.Next(0, 250);
+					displayData[x, y].Color.Blue = ran.Next(0, 250);
+					displayData[x, y].Color.Green = ran.Next(0, 250);
 
 
 				}
-            }
+			}
 
-        }
+		}
 
-        public RGB GetCellColor(int x, int y)
+   
+	}
+
+    
+    public static class Display 
+    {
+
+        public static Pixel[,] GetPixelDataFromGameState(bool[,] gameData)
         {
-            return pixelData[x, y].Color;
+            Pixel[,] pixelData = new Pixel[gameData.GetLength(0),gameData.GetLength(1)];
+
+           for(int y = 0;y < gameData.GetLength(1); y++)
+           {
+				for (int x = 0; x < gameData.GetLength(0); x++)
+                {
+                    pixelData[x, y] = new Pixel(x,y);
+                    if (gameData[x, y]) {  pixelData[x, y].SetColor(250,250,250); }
+                    else { pixelData[x, y].SetColor(0, 0, 0); }
+
+                }
+
+
+		   }
+
+            return pixelData;
         }
 
-    }
+		public static RGB GetCellColor(Pixel[,] displayData, int x, int y)
+		{
+			return displayData[x, y].Color;
+		}
+	}
+   
+    //public class DisplayClass
+    //{
+    //    private int displyX;
+    //    private int displayY;  
+    //    public Pixel[,] pixelData;
 
-    public class Pixel
+    //    public DisplayClass()
+    //    {
+    //        displyX = 60;
+    //        displayY = 60;
+
+    //        pixelData = new Pixel[displyX,displayY];
+    //        InitialiseDisplay();
+    //    }
+       
+
+    //    void InitialiseDisplay()
+    //    {
+
+    //        for ( int y = 0; y < displayY; y++ )
+    //        { 
+    //          for( int x = 0; x < displyX; x++ )
+    //            {
+    //                pixelData[x, y] = new Pixel(x,y);
+
+    //                //Randomize Color 
+    //                Random ran = new Random();
+    //                pixelData[x, y].Color.Red = ran.Next(0, 250);
+				//	pixelData[x, y].Color.Blue = ran.Next(0, 250);
+				//	pixelData[x, y].Color.Green = ran.Next(0, 250);
+
+
+				//}
+    //        }
+
+    //    }
+
+
+    //    public RGB GetCellColor(int x, int y)
+    //    {
+    //        return pixelData[x, y].Color;
+    //    }
+
+    //}
+
+    public struct Pixel
     {
 
         public int X;
@@ -68,7 +188,16 @@ namespace FallingSand.Components.Pages
             Y = y;
             Color = new RGB();
         }
+
+        public void SetColor(int red ,int green,int blue)
+        { 
+            Color.Red = red;
+            Color.Green = green;
+            Color.Blue = blue;
+        }
+
     }
+  
     public struct RGB
     {
         public int Red {  get; set; }
@@ -98,5 +227,6 @@ namespace FallingSand.Components.Pages
 
 
     }
+   
     
 }
