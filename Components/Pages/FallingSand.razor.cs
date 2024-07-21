f5f9e51d2e8d12261de03a08a570586ebac572c6
Owain15 <153022268+Owain15@users.noreply.github.com>
2024-07-22 00:55:02 +0100

    Display build is To Slow. Rethink. Random Falling Left Or Right Only Going Right.

--- Components/Pages/FallingSand.razor.cs
+++ Components/Pages/FallingSand.razor.cs
@@ -1,5 +1,6 @@
 using FallingSand.Components.Layout;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Timers;
@@ -14,39 +15,37 @@ namespace FallingSand.Components.Pages
         int GridSizeY = 60;
 
         System.Timers.Timer timer;
-        int RefreshRate = 1000;
+        int RefreshRate = 100;
 
-        List<(int,int)> SandList = new List<(int,int)>();
+        //List<(int,int)> SandList = new List<(int,int)>();
 		bool[,] gameData;
         Pixel[,] displayData;
 
-        public int intervilCount = 50;
-        public string testClockReadOut;
+        Random random = new Random();
+   
+       
 
         protected override void OnInitialized()
         {
-            testClockReadOut = intervilCount.ToString();
-            
-            //timer = new System.Timers.Timer();
 
-            SandList.Clear();
+            //SandList.Clear();
 
             gameData = new bool[GridSizeX,GridSizeY];
           
             displayData = new Pixel[GridSizeX,GridSizeY];
 
+		    InitialiseDisplay();
+
             StartTimmer();
 
-		    InitialiseDisplay();
-           
             //test sand. Remove.
             gameData[10, 10] = true;
 			gameData[10, 11] = true;
-			gameData[11, 10] = true;
-			gameData[11, 11] = true;
+			gameData[20, 10] = true;
+			gameData[20, 11] = true;
 
-		
-			displayData = Display.GetPixelDataFromGameState(gameData);
+		    BuildDisplay();
+			
 		}
 
 
@@ -61,21 +60,17 @@ namespace FallingSand.Components.Pages
         }
 
       
-        public void HandelTimeElapsedEvent(Object source , System.Timers.ElapsedEventArgs e)
+        public void HandelTimeElapsedEvent(Object state , System.Timers.ElapsedEventArgs e)
         {
-		
-			intervilCount++;
-            
-            Console.WriteLine($"intervelCount = {intervilCount}");
-            
-            //base.ShouldRender();
-            //StateHasChanged();
-        }
-        private void SetClockRender() 
-        {
-            Console.WriteLine("testClockReadOut Updated");
-            testClockReadOut = intervilCount.ToString();
-        }
+
+            gameData = UpdateGameData();
+
+            BuildDisplay();
+
+            Render();
+          
+		}
+    
 
 		void InitialiseDisplay()
 		{
@@ -98,7 +93,61 @@ namespace FallingSand.Components.Pages
 
 		}
 
-   
+        void BuildDisplay()    { displayData = Display.GetPixelDataFromGameState(gameData); }
+
+        void Render()   { InvokeAsync(StateHasChanged); }
+
+        bool[,] UpdateGameData()
+        {
+            bool[,] updatedGameData = new bool[GridSizeX, GridSizeY];
+
+            for(int y = 0; y < gameData.GetLength(1); y++)
+            {
+                for(int x = 0; x < gameData.GetLength(0); x++)
+                {
+                    if (gameData[x, y])
+                    {
+                        if( y + 1 > gameData.GetLength(1)-1) { updatedGameData[x, y] = true; }
+                        else if (!gameData[x, y + 1]) { updatedGameData[x, y + 1] = true;}
+                        else if (gameData[x, y + 1]) {updatedGameData = PileSand(x,y, gameData,updatedGameData); }
+                    }
+                }
+
+            }
+            return updatedGameData;
+
+        }
+
+        bool[,] PileSand(int x, int y, bool[,] gameData, bool[,] updatedGameData)
+        {
+
+            var tryLeftFirst = true;
+            if (random.Next() > 0.5) { tryLeftFirst = false; }
+
+            if (x - 1 >= 0 && tryLeftFirst)
+            {
+
+                if (!gameData[x - 1, y + 1]) { updatedGameData[x - 1, y + 1] = true; }
+                else if(x + 1 <= gameData.GetLength(0) - 1 && !gameData[x + 1, y + 1])
+                { updatedGameData[x + 1, y + 1] = true; }
+                else{ updatedGameData[x, y] = true; }
+            }
+            else if (x + 1 <= gameData.GetLength(0)-1 && !tryLeftFirst)
+            {
+				if (x + 1 <= gameData.GetLength(0) - 1 && !gameData[x + 1, y + 1])
+				{ updatedGameData[x + 1, y + 1] = true; }
+                else if (x -1 >=0 &&!gameData[x - 1, y + 1]) { updatedGameData[x - 1, y + 1] = true; }
+				else { updatedGameData[x, y] = true; }
+
+			}
+            else { Console.WriteLine("ERORR.NextSandLocationNotFound!"); }
+              
+          
+
+            return updatedGameData;
+        }
+
+        void AddSand(int x, int y){ if (!gameData[x, y]) { gameData[x, y] = true; } }   
 	}
 
     
@@ -131,50 +180,7 @@ namespace FallingSand.Components.Pages
 		}
 	}
    
-    //public class DisplayClass
-    //{
-    //    private int displyX;
-    //    private int displayY;  
-    //    public Pixel[,] pixelData;
-
-    //    public DisplayClass()
-    //    {
-    //        displyX = 60;
-    //        displayY = 60;
-
-    //        pixelData = new Pixel[displyX,displayY];
-    //        InitialiseDisplay();
-    //    }
-       
-
-    //    void InitialiseDisplay()
-    //    {
-
-    //        for ( int y = 0; y < displayY; y++ )
-    //        { 
-    //          for( int x = 0; x < displyX; x++ )
-    //            {
-    //                pixelData[x, y] = new Pixel(x,y);
-
-    //                //Randomize Color 
-    //                Random ran = new Random();
-    //                pixelData[x, y].Color.Red = ran.Next(0, 250);
-				//	pixelData[x, y].Color.Blue = ran.Next(0, 250);
-				//	pixelData[x, y].Color.Green = ran.Next(0, 250);
-
-
-				//}
-    //        }
-
-    //    }
-
-
-    //    public RGB GetCellColor(int x, int y)
-    //    {
-    //        return pixelData[x, y].Color;
-    //    }
-
-    //}
+   
 
     public struct Pixel
     {
